c4e128fddb544b4bde0059d872c66c4bcaebbed9
tyltr <tylitianrui@126.com>
2019-09-04 06:07:57 +0800

    leecode 第21题:合并两个有序链表

---
+++ leecode/0021/main.go
@@ -0,0 +1,32 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	p := &ListNode{}
+	l := p
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			l.Next = l1
+			l1 = l1.Next
+
+		} else {
+			l.Next = l2
+			l2 = l2.Next
+		}
+
+		l = l.Next
+	}
+	if l1 == nil {
+		l.Next = l2
+	}
+	if l2 == nil {
+		l.Next = l1
+	}
+	p = p.Next
+	return p
+
+}
