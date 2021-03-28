2e9b197fa3ca17e555570d62085316cd3f7c58ba
tyltr <tianrui_li@qingsongchou.com>
2021-03-28 09:39:34 +0800

    merge

--- basic/0004.归并排序/main.go
+++ basic/0004.归并排序/main.go
@@ -2,33 +2,50 @@ package main
 
 import "fmt"
 
-func merge(left, rigth []int) []int {
-	i, j := 0, 0
-	var res = make([]int, 0)
-	for {
+// func merge(left, rigth []int) []int {
+// 	i, j := 0, 0
+// 	var res = make([]int, 0)
+// 	for {
 
-		if i == len(left) {
-			res = append(res, rigth[j:]...)
-			return res
-		}
-		if j == len(rigth) {
-			res = append(res, left[i:]...)
-			return res
-		}
+// 		if i == len(left) {
+// 			res = append(res, rigth[j:]...)
+// 			return res
+// 		}
+// 		if j == len(rigth) {
+// 			res = append(res, left[i:]...)
+// 			return res
+// 		}
 
-		if left[i] <= rigth[j] {
-			res = append(res, left[i])
-			i++
-			continue
-		}
-		if left[i] > rigth[j] {
-			res = append(res, rigth[j])
-			j++
-			continue
-		}
+// 		if left[i] <= rigth[j] {
+// 			res = append(res, left[i])
+// 			i++
+// 			continue
+// 		}
+// 		if left[i] > rigth[j] {
+// 			res = append(res, rigth[j])
+// 			j++
+// 			continue
+// 		}
 
+// 	}
+
+// }
+
+func merge(left, right []int) []int {
+	l, r := 0, 0
+	result := make([]int, 0)
+	for l < len(left) && r < len(right) {
+		if left[l] <= right[r] {
+			result = append(result, left[l])
+			l++
+		} else {
+			result = append(result, right[r])
+			r++
+		}
 	}
-
+	result = append(result, left[l:]...)
+	result = append(result, right[r:]...)
+	return result
 }
 
 func mersort(arr []int) []int {
@@ -43,7 +60,9 @@ func mersort(arr []int) []int {
 }
 
 func main() {
-	l := []int{3, 1, 3, 54, 6, 2, 9, 45, 4, 56}
+	l := []int{3, 1, 12, 3, 54, 6, 2, 9, 45, 4, 56}
+	r := []int{3}
+	fmt.Println(merge(l, r))
 	fmt.Println(mersort(l))
 
 }
